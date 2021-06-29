b40cd9836c3a63291fab7272ddaaaa9908b7e6f2
muammarahlnn <21muammarahlan@gmail.com>
2021-06-29 18:53:07 +0800

    update action bar

--- app/src/main/java/com/ardnn/mymovies/activities/MainActivity.kt
+++ app/src/main/java/com/ardnn/mymovies/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package com.ardnn.mymovies.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.ardnn.mymovies.R
 import androidx.fragment.app.Fragment
 import com.ardnn.mymovies.fragments.FavoriteFragment
@@ -15,18 +17,22 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     // widgets
+    private lateinit var toolbar: Toolbar
+    private lateinit var tvSection: TextView
+    private lateinit var ivIcon: ImageView
     private lateinit var bnvNavigation: BottomNavigationView
-    private lateinit var tvTitle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // initialize widgets
-        bnvNavigation = findViewById(R.id.bnv_navigation_main)
-        tvTitle = findViewById(R.id.tv_title_main)
+        toolbar = findViewById(R.id.toolbar_main)
+        tvSection = findViewById(R.id.tv_section_toolbar)
+        ivIcon = findViewById(R.id.iv_icon_toolbar)
 
-        // if navigation clicked
+        // set bottom navigation view
+        bnvNavigation = findViewById(R.id.bnv_navigation_main)
         bnvNavigation.setOnNavigationItemSelectedListener(this)
         bnvNavigation.itemIconTintList = null
         bnvNavigation.selectedItemId = R.id.item_movies_main
@@ -36,19 +42,23 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         var selectedFragment: Fragment? = null
         when (item.itemId) {
             R.id.item_movies_main -> {
-                tvTitle.text = resources.getString(R.string.app_name)
+                tvSection.text = resources.getString(R.string.movies)
+                ivIcon.setImageResource(R.drawable.ic_movies_yellow)
                 selectedFragment = MoviesFragment()
             }
             R.id.item_tv_shows_main -> {
-                tvTitle.text = resources.getString(R.string.tv_shows)
+                tvSection.text = resources.getString(R.string.tv_shows)
+                ivIcon.setImageResource(R.drawable.ic_tv_shows_yellow)
                 selectedFragment = TvShowsFragment()
             }
             R.id.item_recent_main -> {
-                tvTitle.text = resources.getString(R.string.recent)
+                tvSection.text = resources.getString(R.string.recent)
+                ivIcon.setImageResource(R.drawable.ic_recent_yellow)
                 selectedFragment = RecentFragment()
             }
             R.id.item_favorite_main -> {
-                tvTitle.text = resources.getString(R.string.favorite)
+                tvSection.text = resources.getString(R.string.favorite)
+                ivIcon.setImageResource(R.drawable.ic_favorite_false)
                 selectedFragment = FavoriteFragment()
             }
         }
