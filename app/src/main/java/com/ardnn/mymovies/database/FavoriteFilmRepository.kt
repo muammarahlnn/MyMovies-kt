20fdbc4ec4a002f5871436168a0d0ec0d59678a8
muammarahlnn <21muammarahlan@gmail.com>
2021-08-02 23:46:27 +0800

    add FavoriteFilmRepository

---
+++ app/src/main/java/com/ardnn/mymovies/database/FavoriteFilmRepository.kt
@@ -0,0 +1,46 @@
+package com.ardnn.mymovies.database
+
+import androidx.lifecycle.LiveData
+import com.ardnn.mymovies.database.dao.FavoriteFilmDao
+import com.ardnn.mymovies.database.entities.FavoriteMovies
+import com.ardnn.mymovies.database.entities.FavoriteTvShows
+
+class FavoriteFilmRepository(private val dao: FavoriteFilmDao) {
+    // favorite movies
+    val favoriteMovieList: LiveData<List<FavoriteMovies>> = dao.getAllMovies()
+
+    suspend fun addMovie(favoriteMovie: FavoriteMovies) {
+        dao.addMovie(favoriteMovie)
+    }
+
+    suspend fun deleteMovie(favoriteMovie: FavoriteMovies) {
+        dao.deleteMovie(favoriteMovie)
+    }
+
+    fun getMovie(id: Int): FavoriteMovies {
+        return dao.getMovie(id)
+    }
+
+    fun isMovieExists(id: Int): Boolean {
+        return dao.isMovieExist(id)
+    }
+
+    // tv shows
+    val favoriteTvShowList: LiveData<List<FavoriteTvShows>> = dao.getAllTvShows()
+
+    suspend fun addTvShow(favoriteTvShow: FavoriteTvShows) {
+        dao.addTvShow(favoriteTvShow)
+    }
+
+    suspend fun deleteTvShow(favoriteTvShow: FavoriteTvShows) {
+        dao.deleteTvShow(favoriteTvShow)
+    }
+
+    fun getTvShow(id: Int): FavoriteTvShows {
+        return dao.getTvShow(id)
+    }
+
+    fun isTvShowExists(id: Int): Boolean {
+        return dao.isTvShowExists(id)
+    }
+}
