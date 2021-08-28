4eb2e5a27f253373fb001f965d69210e108d5c3c
muammarahlnn <21muammarahlan@gmail.com>
2021-08-29 01:44:24 +0800

    tidy up code

--- app/src/main/java/com/ardnn/mymovies/database/AppDatabase.kt
+++ app/src/main/java/com/ardnn/mymovies/database/AppDatabase.kt
@@ -28,19 +28,30 @@ abstract class AppDatabase: RoomDatabase() {
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
             synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "app_database"
-                ).build()
-                INSTANCE = instance
-                return instance
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "app_database"
+                    ).build()
+                }
+                return INSTANCE as AppDatabase
             }
+
+//            val tempInstance = INSTANCE
+//            if (tempInstance != null) {
+//                return tempInstance
+//            }
+//            synchronized(this) {
+//                val instance = Room.databaseBuilder(
+//                    context.applicationContext,
+//                    AppDatabase::class.java,
+//                    "app_database"
+//                ).build()
+//                INSTANCE = instance
+//                return instance
+//            }
         }
     }
 }
